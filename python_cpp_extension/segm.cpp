c7d404fb361dd92b3bddc0b4f7429a53816979ee
Sergey Mikhno (Galenica) <sergey.mikhno@galenica.com>
2022-03-14 17:00:55 +0100

    read numpy array and get_glyphs

--- python_cpp_extension/segm.cpp
+++ python_cpp_extension/segm.cpp
@@ -1,9 +1,43 @@
 #include <Python.h>
+#include <numpy/ndarraytypes.h>
+#include <numpy/ufuncobject.h>
+#include <numpy/npy_3kcompat.h>
 #include "PageSegmenter.h"
 
 // This is the definition of a method
-static PyObject* reflow(PyObject *self, PyObject *args) {
-    return PyLong_FromLong(0);
+static PyObject* reflow(PyObject* self, PyObject *args) {
+    PyObject *input;
+    PyArray_Descr *dtype = NULL;
+    if (!PyArg_ParseTuple(args, "OO&", &input, PyArray_DescrConverter, &dtype)) {
+        return NULL;
+    }
+
+
+    int nd = PyArray_NDIM(input);
+    npy_intp* dims = PyArray_DIMS(input);
+
+    PyArrayObject* contig = (PyArrayObject*)PyArray_FromAny(input,
+        dtype,
+        1, 3, NPY_ARRAY_CARRAY, NULL);
+
+    if (nd >= 2) {
+
+        cv::Mat mat = cv::Mat(cv::Size(dims[1], dims[0]), CV_8UC1, PyArray_DATA(contig));
+        cv::bitwise_not(mat, mat);
+        cv::threshold(mat, mat, 0, 255, cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
+        PageSegmenter ps(mat);
+        std::vector<glyph> glyphs = ps.get_glyphs();
+        cv::imwrite("test.jpg", mat);
+        Py_DECREF(contig); 
+    }
+
+
+/* Use mat here */
+
+
+    Py_INCREF(input);
+    Py_INCREF(dtype);
+    return input;
 }
 
 // Exported methods are collected in a table
@@ -27,5 +61,6 @@ PyModuleDef segm_module = {
 
 // The module init function
 PyMODINIT_FUNC PyInit_segm(void) {
+    import_array();
     return PyModule_Create(&segm_module);
 }
