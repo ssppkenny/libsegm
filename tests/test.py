199d4ca049788a6ff774931d0ca50ea8f14c5d18
Sergey Mikhno <sergey.mikhno@gmail.com>
2022-04-17 09:42:06 +0200

    testing for specific letter e

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@ import cv2
 
 
 def test_join():
-    img = cv2.imread('vd_p214.png')
+    img = cv2.imread('vd_p214.png', cv2.IMREAD_GRAYSCALE)
     joined_rects = join_rects(img)
     assert (len(joined_rects) > 0)
+    letter_e = [r for r in joined_rects if r.x == 2885 and r.y == 3755 and r.width == 33 and r.height == 43][0]
