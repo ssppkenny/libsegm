197f189b881d377b5fb9707f394ddffc7353c3b3
Sergey Mikhno <sergey.mikhno@gmail.com>
2022-04-24 15:31:09 +0200

    test for find_neighbors

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
-from segm import join_rects, join_intervals
+from segm import join_rects, join_intervals, glyph_result, find_neighbors
 import cv2
+import numpy as np
 
 
 def test_join():
@@ -14,3 +15,30 @@ def test_join_intervals():
     lst = join_intervals(rngs)
     assert(lst[0][0] == 0)
     assert(lst[0][1] == 5)
+
+
+def test_find_neighbors():
+    r1 = glyph_result((1, 1, 1, 1))
+    r2 = glyph_result((3, 1, 1, 1))
+    r3 = glyph_result((5, 1, 1, 1))
+    r4 = glyph_result((1, 3, 1, 1))
+    r5 = glyph_result((3, 3, 1, 1))
+    r6 = glyph_result((5, 3, 1, 1))
+    nn = np.array(
+        [
+            [0, 1, 3, 4, 2, 5],
+            [1, 0, 2, 4, 3, 5],
+            [2, 1, 5, 4, 0, 3],
+            [3, 0, 4, 1, 5, 2],
+            [4, 3, 5, 1, 0, 2],
+            [5, 2, 4, 1, 3, 0],
+        ]
+    )
+
+    d = find_neighbors([r1, r2, r3, r4, r5, r6], nn)
+    assert d[2] is None
+    assert d[5] is None
+    assert d[0][1] == 1
+    assert d[1][1] == 2
+    assert d[3][1] == 4
+    assert d[4][1] == 5
